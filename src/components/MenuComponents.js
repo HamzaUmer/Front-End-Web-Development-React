0533f74201b925481bb21ffa06cfb5e1f354370e
HamzaUmer <mirzasahab123321@gmail.com>
2021-08-05 16:36:46 +0500

    React Router

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
   //1st Method of Functional Component
   function RenderMenuItem({dish, onClick}){
   	    return (
-            <Card onClick={() => onClick(dish.id)}>
+            <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
