448d39c7c7e9da0ebac714aee8ba4b6c8276c0a9
HamzaUmer <mirzasahab123321@gmail.com>
2021-08-07 13:10:26 +0500

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponents';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -36,6 +37,14 @@ class Main extends Component {
       );
     }
 
+        const AboutUsPage = () => {
+            return(
+                <About 
+                    leaders={this.state.leaders}
+                />
+            );
+        };
+
     const DishWithId = ({match}) => {
         return(
           <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
@@ -51,6 +60,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={AboutUsPage} />
           <Redirect to= "/home" />
          </Switch>
         <Footer />
--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
   //1st Method of Functional Component
   function RenderMenuItem({dish, onClick}){
   	    return (
+            
             <Card>
               <Link to={`/menu/${dish.id}`} >
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
