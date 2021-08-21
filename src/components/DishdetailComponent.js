57a8d9eecbeb94f7d721085610fff531744599fe
HamzaUmer <mirzasahab123321@gmail.com>
2021-08-21 19:41:36 +0500

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -9,6 +9,7 @@ import {
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 /**........................ comment component ends ................................................. */
@@ -174,6 +175,11 @@ class CommentForm extends Component {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
+            <FadeTransform
+               in 
+               transformProps={{
+                     exitTransform: 'scale(0.5) translateY(-50%)'
+               }}>
                     <Card>
                         <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
@@ -181,6 +187,7 @@ class CommentForm extends Component {
                             <CardText> {dish.description} </CardText>
                         </CardBody>
                     </Card>
+            </FadeTransform>
                 </div>   
             );
         }
@@ -195,9 +202,10 @@ class CommentForm extends Component {
         if (comments == null) {
             return (<div></div>)
         }
+        
         const cmnts = comments.map(comment => {
             return (
-                
+                <Fade in>
                 <li key={comment.id}>
                     <p>{comment.comment}</p>
                     <p>-- {comment.author},
@@ -209,14 +217,17 @@ class CommentForm extends Component {
                     }).format(new Date(comment.date))}
                     </p>
                 </li>
-
-            )
+                </Fade>
+            );
         })
+        
         return (
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
+                <Stagger in>
                     {cmnts}
+                </Stagger>
                 </ul>
                 <CommentForm dish={dish} comments={comments} dishId={dishId} postComment={postComment} />
             </div>
